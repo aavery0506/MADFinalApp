d39a981a8769141f7e130e07c7ce188d36d01f87
aavery0506 <avery.alison.s@gmail.com>
2025-04-07 19:04:48 -0400

    UI

--- app/src/main/java/com/example/selfcareapp/LoginScreen.java
+++ app/src/main/java/com/example/selfcareapp/LoginScreen.java
@@ -1,6 +1,8 @@
 package com.example.selfcareapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,94 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.selfcareapp.databinding.ActivityLoginScreenBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
 public class LoginScreen extends AppCompatActivity {
 
+    ActivityLoginScreenBinding binding;
+
+    String name, email, password;
+
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_screen);
+
+        binding= ActivityLoginScreenBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = binding.email.getText().toString();
+                password = binding.password.getText().toString();
+                login();
+            }
+        });
+
+        binding.signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name= binding.name.getText().toString();
+                email = binding.email.getText().toString();
+                password = binding.password.getText().toString();
+                signUp();
+            }
+        });
+
+    }
+
+    private void login() {
+        FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(email.trim(),password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(LoginScreen.this,MainActivity.class));
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        finish();
+                    }
+
+                });
+    }
+
+    private void signUp() {
+        FirebaseAuth.getInstance()
+                .createUserWithEmailAndPassword(email.trim(),password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        UserProfileChangeRequest userProfileChangeRequest = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(name).build();
+
+                        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                        assert firebaseUser != null;
+                        firebaseUser.updateProfile(userProfileChangeRequest);
+                        UserModel userModel = new UserModel(FirebaseAuth.getInstance().getUid(),name,email,password);
+                        databaseReference.child(Objects.requireNonNull(FirebaseAuth.getInstance().getUid())).setValue(userModel);
+
+                        startActivity(new Intent(LoginScreen.this,MainActivity.class));
+                        finish();
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/example/selfcareapp/UserModel.java
@@ -0,0 +1,50 @@
+package com.example.selfcareapp;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+public class UserModel {
+    private String userID, userName, userEmail, userPassword;
+
+    public UserModel(String userID, String userName, String userEmail, String userPassword){
+        this.userID = userID;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+}
