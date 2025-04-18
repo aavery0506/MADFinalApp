6a22f9ac0899c5170097db5eb5a5f3630c564257
aavery0506 <avery.alison.s@gmail.com>
2025-04-18 10:02:04 -0400

    Working on timer function

--- app/src/main/java/com/example/selfcareapp/Breathing.java
+++ app/src/main/java/com/example/selfcareapp/Breathing.java
@@ -8,6 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.selfcareapp.databinding.ActivityBreathingBinding;
 
@@ -36,15 +39,17 @@ public class Breathing extends AppCompatActivity implements BreatheSelect.Select
 
     @Override
     public void onButtonClick(String text, int mins) {
-        BreatheAnimation breatheAnimation = (BreatheAnimation) getSupportFragmentManager().findFragmentById(R.id.fcv_animation);
-        if (breatheAnimation != null) {
-            breatheAnimation.updateText(text);
-        }
+        navigateToAnimationFragment();
+    }
+
+    private void navigateToAnimationFragment() {
+        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        navController.navigate(R.id.action_selectionFragment_to_timerFragment);
     }
 
     @Override
     public void goHome(){
-        BreatheAnimation breatheAnimation = (BreatheAnimation) getSupportFragmentManager().findFragmentById(R.id.fcv_animation);
+        BreatheAnimation breatheAnimation = (BreatheAnimation) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         if (breatheAnimation != null) {
             breatheAnimation.goHome();
         }
