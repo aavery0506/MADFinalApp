d6a9456d955240caeecd9cb063d4cd0b78b35fc3
aavery0506 <avery.alison.s@gmail.com>
2025-04-20 21:33:28 -0400

    working on Goals section

--- app/src/main/java/com/example/selfcareapp/GoalModel.java
+++ app/src/main/java/com/example/selfcareapp/GoalModel.java
@@ -4,10 +4,17 @@ package com.example.selfcareapp;
 public class GoalModel {
     //set up class variables
     private static GoalModel instance;
+    private int numDays;
+    private int totalMins;
 
 
     public GoalModel() {
         //empty constructor
+    }
+    //constructor that takes in number of days and total minutes
+    public GoalModel(int numDays, int totalMins){
+        this.numDays = numDays;
+        this.totalMins = totalMins;
     }
 
     //get instance of model or create a new instance
@@ -16,5 +23,16 @@ public class GoalModel {
             instance = new GoalModel();
         }
         return instance;
+    }
+
+    //getters and setters
+    public int getNumDays() {return numDays;}
+    public void setNumDays(int numDays) {this.numDays = numDays;}
+    public int getTotalMins() {return totalMins;}
+    public void setTotalMins(int totalMins) {this.totalMins = totalMins;}
+
+    //Method to calculate the number of minutes per day
+    public int numMinsPerDay(){
+        return this.totalMins/this.numDays;
     }
 }
